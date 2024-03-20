72ed0955c67e491d52676aad5be9279e2a0ecce7
CarlosMantilla <127089527+c123qw@users.noreply.github.com>
2024-03-20 15:47:10 +0100

    Update main.py

--- src/02limpieza/modulos_limp/main.py
+++ src/02limpieza/modulos_limp/main.py
@@ -5,7 +5,17 @@ def main():
 
     # Leer los datos de la tabla 'TablaFinalSinDetalles' que contiene las estadísticas de los jugadores desde 2019-2023
     datos = pd.read_csv("TablaFinalSinDetalles.csv", encoding='utf8', sep=';')
-    preprocess_data(datos)
+    datos = preprocess_player_stats(datos)
+    
+    # Leer los datos de Transfermarket
+    tf_data = pd.read_csv('TransferMarket_completo.csv')
+
+    # Preprocesamiento adicional...
+
+    # Guardar los datos preprocesados en archivos CSV
+    datos.to_csv('DatosFinalesLaLiga.csv')
+    tf_data.to_csv('TransfermarketLimpio.csv')
+
 
 if __name__ == "__main__":
     main()
