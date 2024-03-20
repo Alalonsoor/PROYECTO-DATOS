85017060f43527e2954fc292ec75908d7e6383a8
CarlosMantilla <127089527+c123qw@users.noreply.github.com>
2024-03-20 15:38:46 +0100

    Update main.py

--- src/02limpieza/modulos_limp/main.py
+++ src/02limpieza/modulos_limp/main.py
@@ -1,7 +1,11 @@
 from preprocessing import preprocess_data
 
 def main():
-    preprocess_data()
+    # Instalar las bibliotecas necesarias: unidecode, fuzzywuzzy, python-Levenshtein y joblib
+
+    # Leer los datos de la tabla 'TablaFinalSinDetalles' que contiene las estadísticas de los jugadores desde 2019-2023
+    datos = pd.read_csv("TablaFinalSinDetalles.csv", encoding='utf8', sep=';')
+    preprocess_data(datos)
 
 if __name__ == "__main__":
     main()
