9426a7dd1990e0a854e061cacf55e349ef6eda84
CarlosMantilla <127089527+c123qw@users.noreply.github.com>
2024-03-22 16:44:13 +0100

    Update getData.py

--- src/01adquisicion/modulos_adq/TRANSFERMARKET API/getData.py
+++ src/01adquisicion/modulos_adq/TRANSFERMARKET API/getData.py
@@ -2,19 +2,28 @@ import pandas as pd
 import requests
 
 def get_transfer_data(api_url, la_liga_id):
-    clubs_url = f"{api_url}/competitions/{la_liga_id}/clubs"
-    response_clubs = requests.get(clubs_url)
-    clubs_data = response_clubs.json()
-
-    df_players = pd.DataFrame()
-
-    for club in clubs_data.get("clubs", []):
-        club_id = club.get("id")
-        players_url = f"{api_url}/clubs/{club_id}/players?season_id=2019"
-        response_players = requests.get(players_url)
-        players_data = response_players.json()
-
-        df_players = df_players.append(players_data.get("players", []), ignore_index=True)
-
+    anios = ['2022', '2021', '2020', '2019']
+    dfs = []
+    for anio in anios:
+        print(anio)
+        clubs_url = f"{api_url}/competitions/{la_liga_id}/clubs?season_id={anio}"
+        response_clubs = requests.get(clubs_url)
+        clubs_data = response_clubs.json()
+    
+        df_players = pd.DataFrame()
+    
+        for club in clubs_data.get("clubs", []):
+            club_id = club.get("id")
+            players_url = f"{api_url}/clubs/{club_id}/players?season_id={anio}"
+            response_players = requests.get(players_url)
+            players_data = response_players.json()
+    
+            df_players = pd.DataFrame(players_data.get("players", []))
+            df_players['Equipo'] = club.get('name')
+            df_players['Anio'] = anio
+            dfs.append(df_players)
+            
+    df_players = pd.concat(dfs, ignore_index=True)
+    
     return df_players
 
